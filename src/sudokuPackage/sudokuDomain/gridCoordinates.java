2a56980e22d7d36d35140f37829ea26d4d819dc5
Nicolas Noriega <nicolasn8171@gmail.com>
2021-07-17 03:27:24 -0700

    Did more work in the gridCoordinates.java file.

--- src/sudokuPackage/sudokuDomain/gridCoordinates.java
+++ src/sudokuPackage/sudokuDomain/gridCoordinates.java
@@ -20,5 +20,15 @@ public class gridCoordinates
         return y;
     }
 
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this == obj) { return true; }
 
+        if(obj == null || getClass() != obj.getClass()) { return false; }
+
+
+
+
+    }
 }
