0002db8cc15c71a31ef0d1b75c58728c5a706773
Nicolas Noriega <nicolasn8171@gmail.com>
2021-07-18 00:28:07 -0700

    Updated gameMessages.java file with two new messages

--- src/sudokuPackage/constants/gameMessages.java
+++ src/sudokuPackage/constants/gameMessages.java
@@ -2,5 +2,6 @@ package sudokuPackage.constants;
 
 public class gameMessages
 {
-
+    private static final String gameComplete = "Congratulations! You've won the game";
+    private static final String gameError = "I no worky";
 }
