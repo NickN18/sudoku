0640d87cdf3314338497e75436b7324cab122267
Nicolas Noriega <nicolasn8171@gmail.com>
2021-08-18 16:20:44 -0700

    Updated SudokuTextField.java class

--- src/sudokuPackage/userinterface/SudokuTextField.java
+++ src/sudokuPackage/userinterface/SudokuTextField.java
@@ -1,6 +1,28 @@
 package sudokuPackage.userinterface;
 
-public class SudokuTextField
-{
+import javafx.scene.control.TextField;
 
+public class SudokuTextField extends TextField
+{
+    private final int row;
+    private final int col;
+
+    public SudokuTextField(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() { return row; }
+    public int getCol() { return col; }
+
+    @Override
+    public void replaceText(int i, int j, String str)
+    {
+        if(!str.matches("[0-9]"))
+        {
+
+        }
+
+    }
 }
