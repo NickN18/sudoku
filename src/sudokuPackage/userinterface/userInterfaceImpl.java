e63132cdb73df642b2a31ad419c93601ab745ecc
Nicolas Noriega <nicolasn8171@gmail.com>
2021-09-01 23:48:20 -0700

    Updated userInterfaceImpl class

--- src/sudokuPackage/userinterface/userInterfaceImpl.java
+++ src/sudokuPackage/userinterface/userInterfaceImpl.java
@@ -1,4 +1,46 @@
 package sudokuPackage.userinterface;
 
-public class userInterfaceImpl {
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+
+public class userInterfaceImpl implements IUserInterfaceContract.View, EventHandler<KeyEvent>
+{
+
+        @Override
+        public void handle(KeyEvent keyEvent)
+        {
+
+        }
+
+        @Override
+        public void setListener(IUserInterfaceContract.EventListener listener)
+        {
+
+        }
+
+        @Override
+        public void updateSquare(int row, int col, int input)
+        {
+
+        }
+
+        @Override
+        public void updateBoard(SudokuGame game)
+        {
+
+        }
+
+        @Override
+        public void showDialog(String message)
+        {
+
+        }
+
+        @Override
+        public void showError(String message)
+        {
+
+        }
+    }
+
 }
