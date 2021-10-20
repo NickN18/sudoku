221b72be5658bac6940cb7576bd9adfa633fea7a
Nicolas Noriega <nicolasn8171@gmail.com>
2021-10-19 22:11:37 -0700

    Worked on userInterfaceImpl.java and finished drawTextFields() with the help of a newly created helper function, styleSudokuTile()

--- src/sudokuPackage/userinterface/userInterfaceImpl.java
+++ src/sudokuPackage/userinterface/userInterfaceImpl.java
@@ -1,5 +1,7 @@
 package sudokuPackage.userinterface;
 
+import javafx.geometry.Pos;
+import javafx.scene.layout.Background;
 import sudokuPackage.sudokuDomain.gridCoordinates;
 
 import javafx.event.EventHandler;
@@ -9,6 +11,7 @@ import javafx.scene.input.KeyEvent;
 
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -71,10 +74,65 @@ public class userInterfaceImpl implements IUserInterfaceContract.View, EventHand
 
     }
 
+    /**
+     * Draws textfields for numbers on sudoku boards. Handles the tiles to be passed the numbers into
+     *
+     * @param root
+     */
     private void drawTextFields(Group root)
     {
+        final int xOrigin = 50;
+        final int yOrigin = 50;
+
+        final int X_YDelta = 64;
+
+        //O(n^2) complexity (obviously)
+        for(int xIndex = 0; xIndex < 9; xIndex++)
+        {
+            for(int yIndex = 0; yIndex < 9; yIndex++)
+            {
+                int x = xOrigin + xIndex * X_YDelta;
+                int y = yOrigin + yIndex * X_YDelta;
+
+                SudokuTextField tile = new SudokuTextField(xIndex, yIndex);
+
+                styleSudokuTile(tile, x, y);
+
+                tile.setOnKeyPressed(this);
+
+                textFieldCoordinates.put(new gridCoordinates(xIndex, yIndex), tile);    //Coordinates become key and tile becomes value
+
+                root.getChildren().add(tile);
+
+            }
+        }
 
     }
+
+
+    /**
+     * Creates a single Sudoku tile of size 64x64 pixels and at given coordinates x and y
+     *
+     * @param tile Tile to be stylized
+     * @param x x-coordinate to place tile
+     * @param y y-coordinate to place tile
+     */
+    private void styleSudokuTile(SudokuTextField tile, double x, double y)
+    {
+        Font numberFont = new Font(32);
+
+        tile.setFont(numberFont);
+        tile.setAlignment(Pos.CENTER);
+
+        tile.setLayoutX(x);
+        tile.setLayoutY(y);
+        tile.setPrefHeight(64);
+        tile.setPrefWidth(64);
+
+        tile.setBackground(Background.EMPTY);
+    }
+
+
 
     /**
      * Draws the whole grid. Uses getLine() as a helper function
