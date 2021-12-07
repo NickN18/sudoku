b277744a2f65fd98cf1e4f89418439ae5c589aba
Nicolas Noriega <nicolasn8171@gmail.com>
2021-12-06 21:39:35 -0700

    Created ControlLogic.java, finished work in it for the most part as of now, created and started GameGenerator.java

--- src/sudokuPackage/computationalLogic/GameGenerator.java
+++ src/sudokuPackage/computationalLogic/GameGenerator.java
@@ -1,6 +1,64 @@
 package sudokuPackage.computationalLogic;
 
+import sudokuPackage.sudokuDomain.gridCoordinates;
+import sudokuPackage.sudokuDomain.sudokuMain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GameGenerator
 {
+    public static int[][] getNewGameGrid()
+    {
+        return unsolveGame(getSolvedGame());
+    }
+
+    private static int[][] getSolvedGame()
+    {
+        Random random = new Random(System.currentTimeMillis());
+
+        int[][] newGrid =  new int[sudokuMain.gridBounds][sudokuMain.gridBounds];
+
+        for(int i = 1; i <= sudokuMain.gridBounds; i++)
+        {
+            int allocations = 0;
+            int interrupt = 0;
+
+            List<gridCoordinates> allocTracker = new ArrayList<>();
+
+            int attempt = 0;
+
+            while(allocations < sudokuMain.gridBounds)
+            {
+                if(interrupt > 200)
+                {
+                    allocTracker.forEach(coord -> {
+                        newGrid[coord.getX()][coord.getY()] = 0;
+                    });
+
+                    interrupt = 0;
+                    allocations = 0;
+                    allocTracker.clear();
+                    attempt++;
+
+                    if(attempt > 500)
+                    {
+                        clearArray(newGrid);
+                        attempt = 0;
+                        i = 1;
+                    }
+                }
+            }
+         }
+
+        return new int[0][];
+    }
+
+    public static void clearArray(int[][] newGrid)
+    {
+
+    }
+
 
 }
