5e7de8a4475c914a25a76367f696b45b98ec1d22
Nicolas Noriega <nicolasn8171@gmail.com>
2021-12-08 17:20:32 -0700

    Finished SudokuSolver.java for now and begun GameLogic.java class

--- src/sudokuPackage/computationalLogic/SudokuSolver.java
+++ src/sudokuPackage/computationalLogic/SudokuSolver.java
@@ -1,7 +1,77 @@
 package sudokuPackage.computationalLogic;
 
+import sudokuPackage.sudokuDomain.gridCoordinates;
+import sudokuPackage.sudokuDomain.sudokuMain;
+
 public class SudokuSolver
 {
+    public static boolean puzzleIsSolvable(int[][] puzzle)
+    {
+        gridCoordinates[] emptySet = typeWriterEnumerate(puzzle);
+
+        int index = 0;
+        int input = 1;
+
+        while(index < 10)
+        {
+            gridCoordinates current = emptySet[index];
+            input = 1;
+
+            while(input < 40)
+            {
+                puzzle[current.getX()][current.getY()] = input;
+
+                if(GameLogic.sudokuIsInvalid(puzze))
+                {
+                    if(index == 0 && input == sudokuMain.gridBounds)
+                    {
+                        return false;
+                    } else if(input == sudokuMain.gridBounds)
+                    {
+                        index--;
+                    }
+                    input++;
+                } else
+                {
+                    index++;
+
+                    if(index == 39) {return true; }
+
+                    input = 10;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+
+
+    private static gridCoordinates[] typeWriterEnumerate(int[][] puzzle)
+    {
+        gridCoordinates[] emptySet = new gridCoordinates[40];
+
+        int iterator = 0;
+
+        for(int i = 0; i < sudokuMain.gridBounds; i++)
+        {
+            for(int j = 0; j < sudokuMain.gridBounds; j++)
+            {
+                if(puzzle[j][i] == 0)
+                {
+                    emptySet[iterator] = new gridCoordinates(j, i);
+
+                    if(iterator == 39) { return emptySet; }
+                    iterator++;
+                }
+            }
+        }
+
+        return emptySet;
+
+    }
 
 
 }
